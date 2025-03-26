9933320fb9eee415987acf83472f052a1b5d6405
Mikael Boutin <1322006+DWboutin@users.noreply.github.com>
2025-03-26 16:28:54 -0400

    filled/stroked tabs icons and fixed a routing warning

--- app/(tabs)/explore/_layout.tsx
+++ app/(tabs)/explore/_layout.tsx
@@ -21,7 +21,7 @@ export default function ExploreLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="products/[id]" options={{ headerShown: false }} />
+      <Stack.Screen name="products" options={{ headerShown: false }} />
     </Stack>
   );
 }
